121a2acafe005040f29d7c8ce6e315fdcf83d25f
Rukin Danil <rukin2018d@gmail.com>
2023-06-09 17:46:12 +0700

    data profiles

---
+++ src/Sibers.ProjectManagementSystem.Data/DataProfiles/IDataProfileFactory.cs
@@ -0,0 +1,14 @@
+﻿using Sibers.ProjectManagementSystem.Data.DataProfiles.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibers.ProjectManagementSystem.Data.DataProfiles
+{
+    public interface IDataProfileFactory
+    {
+        DataProfile CreateProfile();
+    }
+}
---
+++ src/Sibers.ProjectManagementSystem.Data/DataProfiles/SqlServerDatabaseProfile.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sibers.ProjectManagementSystem.Data.DataProfiles.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sibers.ProjectManagementSystem.Data.DataProfiles
+{
+    public class SqlServerDatabaseProfile : DataProfile
+    {
+        public SqlServerDatabaseProfile(string name, string connectionString, bool useSeedData, bool migrateDatabase, bool createDatabase) :
+            base(name, connectionString, useSeedData, migrateDatabase, createDatabase)
+        {
+        }
+
+        public override void ConfigureDbContextOptionsBuilder(DbContextOptionsBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
