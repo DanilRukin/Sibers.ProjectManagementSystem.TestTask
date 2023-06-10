45051b8ffd4c5a0553692327d6dafbeddca457f4
Rukin Danil <rukin2018d@gmail.com>
2023-06-10 14:45:02 +0700

    employees controller

---
+++ src/Sibers.ProjectManagementSystem.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,97 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sibers.ProjectManagementSystem.Api.Services;
+using Sibers.ProjectManagementSystem.Application.EmployeeAgregate.Commands;
+using Sibers.ProjectManagementSystem.Application.EmployeeAgregate.Queries;
+using Sibers.ProjectManagementSystem.Application.EmployeeAgregate;
+
+namespace Sibers.ProjectManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public EmployeesController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<EmployeeDto>> Create([FromBody] EmployeeDto employee)
+        {
+            CreateEmployeeCommand request = new(employee);
+            var response = await _mediator.Send(request);
+            if (response.IsSuccess)
+                return Created("", response.Value);
+            else
+                return ResultErrorsHandler.Handle(response);
+        }
+
+        [HttpGet("{id:int}/{includeAdditionalData:bool}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<EmployeeDto>> GetById(int id, bool includeProjects = false)
+        {
+            GetEmployeeByIdQuery request = new(new EmployeeIncludeOptions(id, includeProjects));
+            var response = await _mediator.Send(request);
+            if (response.IsSuccess)
+                return Ok(response.Value);
+            else
+                return ResultErrorsHandler.Handle(response);
+        }
+
+        [HttpGet("range/{includeAdditionalData:bool}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetRangeOfEmployees([FromBody] IEnumerable<EmployeeIncludeOptions> options)
+        {
+            GetRangeOfEmployeesQuery query = new GetRangeOfEmployeesQuery(options);
+            var response = await _mediator.Send(query);
+            if (response.IsSuccess)
+                return Ok(response.Value);
+            else
+                return ResultErrorsHandler.Handle(response);
+        }
+
+        [HttpGet("all/{includeAdditionalData:bool}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetALl([FromRoute] bool includeProjects = false)
+        {
+            GetAllEmployeesQuery query = new(includeProjects);
+            var response = await _mediator.Send(query);
+            if (response.IsSuccess)
+                return Ok(response.Value);
+            else
+                return ResultErrorsHandler.Handle(response);
+        }
+
+        [HttpPut("update")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<EmployeeDto>> Update([FromBody] EmployeeDto employee)
+        {
+            UpdateEmployeesDataCommand command = new(employee);
+            var response = await _mediator.Send(command);
+            if (response.IsSuccess)
+                return Ok(response.Value);
+            else
+                return ResultErrorsHandler.Handle(response);
+        }
+    }
+}
