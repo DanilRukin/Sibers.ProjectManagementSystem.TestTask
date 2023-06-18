9a6d746c0047f4b27e982022cee4375b6e21e921
Rukin Danil <rukin2018d@gmail.com>
2023-06-18 22:45:57 +0700

    redirect to login component

---
+++ src/Sibers.ProjectManagementSystem.Presentation.Blazor/Components/RedirectToLoginComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Sibers.ProjectManagementSystem.Presentation.Blazor.Components
+{
+    public partial class RedirectToLoginComponent
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.NavigateTo("/login"); ;
+        }
+    }
+}
