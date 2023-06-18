d55f000ae502894ec209438c00859b3126373d18
Rukin Danil <rukin2018d@gmail.com>
2023-06-18 20:52:29 +0700

    login page

---
+++ src/Sibers.ProjectManagementSystem.Presentation.Blazor/Infrastructure/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sibers.ProjectManagementSystem.Presentation.Blazor.Infrastructure.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Это обязательное поле")]
+        [StringLength(50, ErrorMessage = "Слишком длинное имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Это обязательное поле")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; } = false;
+    }
+}
---
+++ src/Sibers.ProjectManagementSystem.Presentation.Blazor/Pages/Login.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Sibers.ProjectManagementSystem.Presentation.Blazor.Infrastructure.ViewModels;
+
+namespace Sibers.ProjectManagementSystem.Presentation.Blazor.Pages
+{
+    public partial class Login
+    {
+        public LoginViewModel LoginViewModel { get; set; } = new LoginViewModel();
+
+        private async Task LoginAsync(EditContext context)
+        {
+
+        }
+    }
+}
